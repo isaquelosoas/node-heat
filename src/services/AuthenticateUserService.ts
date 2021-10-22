092b30b23ac296ad66b8114f1c142579baecd53f
isaquelosoas <60038289+isaquelosoas@users.noreply.github.com>
2021-10-22 06:57:48 -0300

    jhkhjk

--- src/services/AuthenticateUserService.ts
+++ src/services/AuthenticateUserService.ts
@@ -1,9 +1,18 @@
 import axios from "axios"
+interface IAuthenticateResponse{
+    access_token:string
+}
+interface IUserDataResponse{
+    name:string,
+    id:string,
+    avatar_url:string,
+    login:string
+}
 
 class AuthenticateUserService{
     async execute(code:String){
         const url = "https://github.com/login/oauth/access_token"
-        const response = await axios.post(url, null, {
+        const {data} = await axios.post<IAuthenticateResponse>(url, null, {
             params:{
                 client_id:process.env.GITHUB_CLIENT_ID,
                 client_secret:process.env.GITHUB_CLIENT_SECRET,
@@ -11,6 +20,12 @@ class AuthenticateUserService{
             },
             headers:{
                 "Accept":"application/json"
+            }
+        })
+        console.log(data.access_token)
+        const response = await axios.get<IUserDataResponse>("https://api.github.com/user",{
+            headers:{
+                authorization:`Bearer ${data.access_token}`
             }
         })
         return response.data
